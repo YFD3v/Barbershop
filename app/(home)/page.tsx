8d09a86daf99d4e1141a101276785b7f41dbe590
Yan Ferreira <fabioyan2@gmail.com>
2024-02-17 16:17:34 -0300

    Criação do componente de agendamentos

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import Header from "../_components/Header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
+import BookingItem from "../_components/BookingItem";
 
 export default function Home() {
   return (
@@ -17,7 +18,13 @@ export default function Home() {
         </p>
       </div>
       <div className="px-5 mt-6">
-      <Search />
+        <Search />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-sm uppercase text-gray-400 font-bold mb-3">
+          Agendamentos
+        </h2>
+        <BookingItem />
       </div>
     </div>
   );
